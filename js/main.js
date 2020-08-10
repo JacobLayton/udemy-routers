ee8eefed99d5b96211e8dc84c83d14e2b5349401
JacobLayton <jlayjlay3@gmail.com>
2020-08-10 16:23:07 -0700

    Router created

--- js/main.js
+++ js/main.js
@@ -21,3 +21,35 @@ var GenresView = Backbone.View.extend({
     return this;
   },
 });
+
+var AppRouter = Backbone.Router.extend({
+  routers: {
+    albums: "viewAlbums",
+    "albums/:albumId": "viewAlbumById",
+    artists: "viewArtists",
+    genres: "viewGenres",
+    "*other": "defaultRoute",
+  },
+
+  viewArtists: function () {
+    var view = new ArtistsView({ el: "#container" });
+    view.render();
+  },
+
+  viewGenres: function () {
+    var view = new GenresView({ el: "#container" });
+    view.render();
+  },
+
+  defaultRoute: function () {},
+
+  viewAlbumById: function (albumId) {},
+
+  viewAlbums: function () {
+    var view = new AlbumsView({ el: "#container" });
+    view.render();
+  },
+});
+
+var router = new AppRouter();
+Backbone.history.start();
