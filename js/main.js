879a2a4e118758e9d3d095ffa31b9ead659ec0ff
JacobLayton <jlayjlay3@gmail.com>
2020-08-10 16:05:12 -0700

    Views crated for artist, album, and genre

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+var ArtistsView = Backbone.View.extend({
+  render: function () {
+    this.$el.html("ARTISTS VIEW");
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+    return this;
+  },
+});
+
+var AlbumsView = Backbone.View.extend({
+  render: function () {
+    this.$el.html("ALBUMS VIEW");
+
+    return this;
+  },
+});
+
+var GenresView = Backbone.View.extend({
+  render: function () {
+    this.$el.html("GENRES VIEW");
+
+    return this;
+  },
+});
